c7b2e74e5389b1f94b0796e48f58478eba5c6bff
nafiurbjit <nafiur.rashid@bjitgroup.com>
2023-10-11 17:07:31 +0600

    create: nat-gw created

--- modules/networking/networking.tf
+++ modules/networking/networking.tf
@@ -50,7 +50,7 @@ resource "aws_subnet" "private" {
     cidr_block = var.priv_ciders[count.index]
     availability_zone = local.azs[count.index]
     tags ={
-      Name = "Public-${count.index + 1}" 
+      Name = "Private-${count.index + 1}" 
     }   
 }
 
@@ -62,18 +62,26 @@ resource "aws_subnet" "private" {
 resource "aws_internet_gateway" "main" {
   vpc_id =aws_vpc.this_vpc.id 
 }
+##################################################
+# Create Nat-Gateway
+################################################################################
+
+
+resource "aws_nat_gateway" "this" {
+  connectivity_type = "private"
+  # allocation_id = aws_eip.nat_eip.id
+  #aws_eip
+  # count=length(var.priv_ciders) 
+  subnet_id =  aws_subnet.private[0].id
+}
 
 ################################################################################
-# Create Route Table
+# Create Public Route Table
 ################################################################################
 
-# Create Route Table 
+# Create public Route Table 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.this_vpc.id
-
-################################################################################
-# Attach Route Table with Interenet-Gateway
-################################################################################
 
 # Attach route table with IGW
   route {
@@ -87,7 +95,33 @@ resource "aws_route_table" "public" {
 }
 
 ################################################################################
-# Associate Route-Table
+# Create Private Route Table
+################################################################################
+
+# Create private Route Table 
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+  tags = {
+    Name = "private-route-table"
+  }
+}
+################################################################################
+# Associate Route-Table (private)
+################################################################################
+resource "aws_route_table_association" "private_route_table_association" {
+  route_table_id = aws_route_table.private.id
+  count=length(var.priv_ciders) 
+  subnet_id =  aws_subnet.private[count.index].id
+}
+
+
+
+################################################################################
+# Associate Route-Table (public)
 ################################################################################
 
 # Associate route-Table with public subnet
@@ -96,4 +130,16 @@ resource "aws_route_table_association" "public" {
   subnet_id      = local.pub_sub_ids[count.index]  
   route_table_id = aws_route_table.public.id
 }
+
+
+
+resource "aws_eip" "nat_eip" {}
+
+# Create a route in the private route table to send traffic through the NAT gateway
+# resource "aws_route" "private_subnet_route" {
+
+#   route_table_id         = aws_route_table.private.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.this.id
+# }
 
